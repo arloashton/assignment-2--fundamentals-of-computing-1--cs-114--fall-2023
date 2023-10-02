53c9c65e84153a0753d349ded48c5d219018271b
arloashton <143569631+arloashton@users.noreply.github.com>
2023-10-02 08:36:11 -0400

    updated and streamlined even or odd

--- Diamond.java
+++ Diamond.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
     int userSize;
+    String brick;
+
     Scanner sizeScan = new Scanner(System.in);
     System.out.println("Provide an integer for size of diamond");
     userSize = sizeScan.nextInt();
 
     if(userSize % 2 != 0){
-      System.out.println("ODD");
+      brick = "*";
     }
-
+    else{
+      brick = "* ";
+    }
   }
 }
