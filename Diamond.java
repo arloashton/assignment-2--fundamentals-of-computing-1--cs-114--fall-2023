673f6c8cb8ede7347a54fc3f1b7a205a24aaa680
arloashton <143569631+arloashton@users.noreply.github.com>
2023-10-02 08:15:14 -0400

    Scanner for user input
    created scanner to obtain user input

--- Diamond.java
+++ Diamond.java
@@ -1,5 +1,12 @@
 //Arlo Ashton
+
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int userSize;
+    Scanner sizeScan = new Scanner(System.in);
+    System.out.println("Provide an integer for size of diamond");
+    userSize = sizeScan.nextInt();
   }
 }
