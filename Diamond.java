2d1dc93da5ddce6d3e30ab0fa5ff8c4314cdfc88
arloashton <143569631+arloashton@users.noreply.github.com>
2023-10-16 11:54:28 -0400

    Prints Odd Numbers
    program can now print a diamond for an odd integer

--- Diamond.java
+++ Diamond.java
@@ -7,7 +7,7 @@ public class Diamond {
     int userSize, length;
     double radius;
     String brick;
-
+    String filler = " ";
     Scanner sizeScan = new Scanner(System.in);
     System.out.println("Provide an integer for size of diamond");
     userSize = sizeScan.nextInt();
@@ -16,10 +16,39 @@ public class Diamond {
 
     System.out.println(radius);
 
-    if(userSize % 2 != 0){ //check if integer provided is even or odd
-      brick = "*";
+    if(userSize % 2 != 0){ //check if integer provided is odd
+      brick = "*"; //sets the string of which the diamond is built to "*" since the userSize is odd
+      length = 1;
+      while(length < userSize){
+        for(int i = 0; i < (userSize-length)/2; i++){
+          System.out.print(filler);
+        }
+        for(int i = 0; i < length; i++){
+          System.out.print(brick);
+        }
+        System.out.println("");
+        length+=2;
       }
-    else{
+      if(length == userSize){
+        for(int i = 0; i<userSize; i++ ){
+          System.out.print(brick);
+        }
+      }
+      length-=2;
+      System.out.println("");
+
+      while(length>0){
+        for(int i = 0; i < (userSize-length)/2; i++){
+          System.out.print(filler);
+        }
+        for(int i = 0; i < length; i++){
+          System.out.print(brick);
+        }
+        System.out.println("");
+        length-=2;
+      }
+    }
+    else{//check if integer provided is even
       brick = "* ";
 
     }
