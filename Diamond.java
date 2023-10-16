e8cfec36e208d28dc001a4b2e65e8298275b582b
arloashton <143569631+arloashton@users.noreply.github.com>
2023-10-16 11:28:37 -0400

    CleanedUp

--- Diamond.java
+++ Diamond.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int userSize;
+    int userSize, length;
+    double radius;
     String brick;
 
     Scanner sizeScan = new Scanner(System.in);
     System.out.println("Provide an integer for size of diamond");
     userSize = sizeScan.nextInt();
 
-    if(userSize % 2 != 0){
+    radius = Math.floor(userSize/2);
+
+    System.out.println(radius);
+
+    if(userSize % 2 != 0){ //check if integer provided is even or odd
       brick = "*";
-    }
+      }
     else{
       brick = "* ";
+
     }
+
+
   }
 }
